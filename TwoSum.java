56c446821e2fc1b5d5e62431782c89f51dcb594f
Jiyoung Yoon <ebbuni1023@users.noreply.github.com>
2021-01-21 15:27:53 -0600

    using Hashmap

--- TwoSum.java
+++ TwoSum.java
@@ -4,12 +4,19 @@ public class TwoSum{
 
         int [] testResult3 = twoSum(new int[] {0, 2, 3, 4, 5}, 6);
         int [] testResult4 = twoSum(new int[] {1, 2, 3, 4, 5}, 10);
-        
+        int [] testResult5 = twoSumFast(new int[] {1, 2, 3, 4, 5}, 10);
+
         System.out.println(Arrays.toString(testResult3)); // should output [1, 3]
         System.out.println(Arrays.toString(testResult4)); // should output [-1,-1]
+        System.out.println(Arrays.toString(testResult5)); // should output [-1,-1]
 
     }
-
+/**
+ * What's the best case runtime ? O(1)
+ * What's the worst case? O(N^2)
+ * Average case? O(N^2)
+ * How much space does it use?
+ */
 
 public static int[] twoSum(int[] arr, int targetSum){
     for (int i = 0; i < arr.length; i++){
@@ -21,5 +28,28 @@ public static int[] twoSum(int[] arr, int targetSum){
     }
     return new int[]{-1,-1};
     
+}
+//using HashSet
+/**
+ * What's the best case runtime? O(1)
+ * What's the worst case?
+ * Average case?
+ * How much space does it use?
+ */
+public static int[] twoSumFast(int[] arr, int target){
+    HashSet<Integer> seen = new HashSet<>();
+    for (int j = 0; j < arr.length; j++){
+        int otherAddend = target - arr[j];
+        if(seen.contains(otherAddend)){
+            for (int i = 0; i < arr.length; i++){
+                if(arr[i] == otherAddend){
+                    return new int[] {i, j};
+                }
+            }
+        } else {
+            seen.add(arr[j]);
+        }
+    }
+    return new int[] {-1, -1};
 }
 }
